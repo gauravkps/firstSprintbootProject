9d0c275119b87c913f60274aa59431ea8d5c8dcf
gauravkps <gauravk01071999@gmail.com>
2025-01-28 18:19:41 +0530

    TDD1`

--- src/test/java/org/example/Maintest.java
+++ src/test/java/org/example/Maintest.java
@@ -5,22 +5,22 @@ public class Maintest {
     @Test
     public void add() {
         Main main = new Main();
-        int result = main.add(3, 4);
-        assert result == 7;
+        int result = main.add(5, 4);
+        assert result == 9;
     }
 
     @Test
     public void multiply() {
         Main main = new Main();
-        int result = main.multiply(3, 4);
-        assert result == 12;
+        int result = main.multiply(4, 4);
+        assert result == 16;
     }
 
     @Test
     public void subtract() {
         Main main = new Main();
-        int result = main.subtract(5, 3);
-        assert result == 2;
+        int result = main.subtract(4, 3);
+        assert result == 1;
     }
 }
 
